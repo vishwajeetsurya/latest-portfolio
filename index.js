cdd1aafbe48aab83444738a36cdc4b80bcf667da
Vishwaa <vish@gmail.com>
2024-02-17 20:21:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const contactRoutes = require('./routes/contactRoutes');
+require("dotenv").config({ path: "./.env" })
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URL)
+const app = express();
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "dist")))
+
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
+app.use(cors({}))
+const PORT = process.env.PORT || 5000;
+
+
+app.use('/api/contact', contactRoutes);
+
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "Resource Not Found" })
+})
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ message: "something went wrong" })
+})
+
+mongoose.connection.once("open", () => {
+    console.log('MONGO CONNECTED');
+    app.listen(process.env.PORT, console.log("SERVER RUNNING"))
+})
---
+++ models/GetEmail.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const getEmailSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const GetEmail = mongoose.model('GetEmail', getEmailSchema)
+
+module.exports = GetEmail
